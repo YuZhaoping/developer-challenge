cb839f9aecbc65ba4d737bbc13735d51c8650156
Zhaoping Yu <yuzhaoping1970@gmail.com>
2020-11-23 10:22:11 -0500

    Make styles for rating movies' page layout

--- MovieRating/FrontEnd/src/pages/RatingMovies/Layout.js
+++ MovieRating/FrontEnd/src/pages/RatingMovies/Layout.js
@@ -5,15 +5,50 @@ import React from 'react';
 import { withRouter } from "react-router";
 
 
+import makeStyles from '@material-ui/core/styles/makeStyles';
+
+import Typography from '@material-ui/core/Typography';
+
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    overflow: 'hidden',
+    flexDirection: 'column'
+  },
+
+  title: {
+    display: 'flex',
+    flexDirection: 'row'
+  },
+
+  content: {
+    flexGrow: 1
+  }
+}));
+
+
 const Layout = (props) => {
   const { match } = props;
 
   const contactId = match.params.contactId;
 
+  const classes = useStyles();
+
 
   return (
-    <div>
-      { `TODO: ${contactId} movies' page layout` }
+    <div className={ classes.root } >
+
+      <div className={ classes.title } >
+        <Typography variant='h6' >
+          { `Contant: '${contactId}' movies:` }
+        </Typography>
+      </div>
+
+      <div className={ classes.content } >
+
+      </div>
+
     </div>
   );
 };
