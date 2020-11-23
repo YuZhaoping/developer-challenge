4cfc14705931e417eddfc2174ca6ccac25e23700
Zhaoping Yu <yuzhaoping1970@gmail.com>
2020-11-23 08:03:09 -0500

    Setup cors

--- MovieRating/BackEnd/src/app.js
+++ MovieRating/BackEnd/src/app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
+
 
 var indexRouter = require('./routes/index');
 var apisRouter = require('./routes/apis');
@@ -15,12 +17,18 @@ import serviceSupplier from './services/supplier';
 
 app.config = config;
 
+const corsOptions = {
+  credentials: true,
+  origin: config.cors.origin,
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors(corsOptions));
 
 app.use('/', indexRouter);
 app.use('/api/v1', apisRouter);
--- MovieRating/BackEnd/src/config/index.js
+++ MovieRating/BackEnd/src/config/index.js
@@ -2,5 +2,8 @@
 export default {
   server: {
     port: process.env.PORT || '4000'
+  },
+  cors: {
+    origin: 'http://localhost:3000'
   }
 };
