a1c39da4b5fa5f9412d4efab1f590f6eb80c2225
Zhaoping Yu <yuzhaoping1970@gmail.com>
2020-11-26 19:01:59 -0500

    Refactor the mock movie model

--- MovieRating/BackEnd/src/models/mock/movie.js
+++ MovieRating/BackEnd/src/models/mock/movie.js
@@ -6,6 +6,14 @@ const init = async (props) => {
 }
 
 
+const crypto = require('crypto');
+
+function generateMovieId(title) {
+  // TODO, just simulating for test by md5
+  return crypto.createHash('md5').update(title).digest('hex');
+}
+
+
 const newMovie = (movieId, dto) => {
   return {
     movieId,
@@ -16,8 +24,23 @@ const newMovie = (movieId, dto) => {
   };
 };
 
+const addMovieForRating = async (ratingId, movieDTO) => {
+  const movieId = generateMovieId(movieDTO.title);
 
-const scoreMovie = (movie, scoreByUser) => {
+  const movie = newMovie(movieId, movieDTO);
+
+  await dataStore.addMovieForRating(ratingId, movie);
+
+  return movie;
+};
+
+
+const getMovieOfRating = async (ratingId, movieId) => {
+  return await dataStore.findRatingMovieByIds(ratingId, movieId);
+}
+
+
+const accumulateMovieScore = (movie, scoreByUser) => {
   if (scoreByUser >= 0) {
     let totalScore = movie.totalScore;
     let ratedUserCount = movie.ratedUserCount;
@@ -36,11 +59,20 @@ const scoreMovie = (movie, scoreByUser) => {
   }
 }
 
+const rateMovie = async (ratingId, movie, scoreByUser) => {
+  movie = accumulateMovieScore(movie, scoreByUser);
+
+  await dataStore.updateRatingMovie(ratingId, movie);
+
+  return movie;
+}
+
 
 const movieModel = {
   init,
-  newMovie,
-  scoreMovie
+  addMovieForRating,
+  getMovieOfRating,
+  rateMovie
 };
 
 
--- MovieRating/BackEnd/src/services/ratings.js
+++ MovieRating/BackEnd/src/services/ratings.js
@@ -1,14 +1,10 @@
 /* The movie ratings' service */
 
-let ratingsDataStore;
-
 let ratingModel;
 let movieModel;
 
 
 const init = async ({ providers }) => {
-  ratingsDataStore = providers.ratingsDataStore;
-
   const modelSupplier = providers.ratingsModelSupplier;
 
   ratingModel = modelSupplier.ratingModel;
@@ -16,52 +12,21 @@ const init = async ({ providers }) => {
 };
 
 
-const getAllRatings = async () => {
-  return await ratingModel.findAllRatings();
-};
+const getAllRatings = async () => (await ratingModel.findAllRatings());
 
+const createRating = async (ratingDTO) => (await ratingModel.createRating(ratingDTO));
 
-const crypto = require('crypto');
+const getMoviesOfRating = async (ratingId) => (
+  await ratingModel.getMoviesOfRating(ratingId)
+);
 
-function createIdByName(name) {
-  // TODO, just simulating for test by md5
-  return crypto.createHash('md5').update(name).digest('hex');
-}
-
-
-const createRating = async (ratingDTO) => {
-  return await ratingModel.createRating(ratingDTO);
-};
-
-
-const getMoviesOfRating = async (ratingId) => {
-  return await ratingModel.getMoviesOfRating(ratingId);
-};
-
-
-const addMovieForRating = async (ratingId, movieDTO) => {
-  const movieId = createIdByName(movieDTO.title);
-
-  const movie = movieModel.newMovie(movieId, movieDTO);
-
-  await ratingsDataStore.addMovieForRating(ratingId, movie);
-
-  return movie;
-};
-
+const addMovieForRating = async (ratingId, movieDTO) => (
+  await movieModel.addMovieForRating(ratingId, movieDTO)
+);
 
 const scoreMovie = async (ratingId, movieId, scoreByUser) => {
-  let movie = await ratingsDataStore.findRatingMovieByIds(ratingId, movieId);
-
-  if (movie) {
-    movie = movieModel.scoreMovie(movie, scoreByUser);
-
-    await ratingsDataStore.updateRatingMovie(ratingId, movie);
-
-    return movie;
-  } else {
-    return {};
-  }
+  const movie = await movieModel.getMovieOfRating(ratingId, movieId);
+  return movie ? await movieModel.rateMovie(ratingId, movie, scoreByUser) : {};
 };
 
 
