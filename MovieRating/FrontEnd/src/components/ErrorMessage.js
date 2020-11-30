79fc08ab2a5bfacf63704e48d240da54565249f7
Zhaoping Yu <yuzhaoping1970@gmail.com>
2020-11-30 08:58:20 -0500

    Complete the ErrorMessage component

--- MovieRating/FrontEnd/src/components/ErrorMessage.js
+++ MovieRating/FrontEnd/src/components/ErrorMessage.js
@@ -1,14 +1,79 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import SnackbarContent from '@material-ui/core/SnackbarContent';
+import IconButton from '@material-ui/core/IconButton';
 
-const ErrorMessage = (props) => {
-  const { error, isNew } = props;
+import ErrorIcon from '@material-ui/icons/Error';
+import CloseIcon from '@material-ui/icons/Close';
+
+import makeStyles from '@material-ui/core/styles/makeStyles';
+
+
+import { removeError } from '../actions/errors';
+
+
+const useStyles = makeStyles(theme => ({
+  snackbar: {
+    backgroundColor: 'inherit',
+    color: theme.palette.error.dark,
+  },
+  errorIcon: {
+    fontSize: 20,
+    opacity: 0.9,
+    marginRight: theme.spacing(1),
+  },
+  closeIcon: {
+    fontSize: 20,
+  },
+  message: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+}));
+
+
+const MessageBar = (props) => {
+  const { message, onClose } = props;
+
+  const classes = useStyles();
 
 
   return (
+    <SnackbarContent
+      className={ classes.snackbar }
+      aria-describedby="error-message-bar"
+      message={
+        <span id="error-message-bar" className={ classes.message }>
+          <ErrorIcon className={ classes.errorIcon } />
+          { message }
+        </span>
+      }
+      action={[
+        <IconButton key="close" aria-label="close" color="inherit" onClick={ onClose }>
+          <CloseIcon className={ classes.closeIcon } />
+        </IconButton>,
+      ]}
+    />
+  );
+};
+
+
+const ErrorMessage = (props) => {
+  const { error, isNew, removeError } = props;
+
+  const onClose = () => {
+    error && removeError(error);
+  }
+
+  return (
     <div>
-      { error }
+      { error &&
+        <MessageBar
+          message={ error.toString() }
+          onClose={ onClose }
+        />
+      }
     </div>
   );
 };
@@ -21,4 +86,5 @@ const mapStateToProps = ({ errorsProfile }) => {
 };
 
 export default connect(mapStateToProps, {
+  removeError
 })(ErrorMessage);
