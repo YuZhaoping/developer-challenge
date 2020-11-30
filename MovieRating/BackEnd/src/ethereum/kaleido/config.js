e1680f5589ca19933c512a40245ce009db7abc2c
Zhaoping Yu <yuzhaoping1970@gmail.com>
2020-11-30 12:26:55 -0500

    Commit the config for testing

--- MovieRating/BackEnd/src/ethereum/kaleido/config.js
+++ MovieRating/BackEnd/src/ethereum/kaleido/config.js
@@ -4,7 +4,7 @@ export default {
   // ** Make sure you take the "-connect" URL for your node here, which is the REST API Gateway **
   KALEIDO_REST_GATEWAY_URL: "https://u0pr71jj7q-u0wx7jgktr-connect.us0-aws.kaleido.io/",
   KALEIDO_AUTH_USERNAME: "u0ev3dnly8",
-  KALEIDO_AUTH_PASSWORD: "u6ertgqF5YfAXSn4ZvDosq6mExq9nyvspIo8gEJq6yI",
+  KALEIDO_AUTH_PASSWORD: "hFeGnJn230xeFuT6V6r0ZE0AWVDMBKDKaHFJ2vmQDHA",
   // The "from" address to sign the transactions. Must exist in the node's wallet
   FROM_ADDRESS: "0x4b983da8fae69850c0c479596d83bb302ebb183d",
   CONTRACT_MAIN_SOURCE_FILE: "MoviesRating.sol",
