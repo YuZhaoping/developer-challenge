da7f19c999d64baa4a1ba0d114a85f4f1ed683a8
Zhaoping Yu <yuzhaoping1970@gmail.com>
2020-12-01 22:23:22 -0500

    Add the backend docker compose scripts

---
+++ MovieRating/BackEnd/docker/compose-down.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+cd $(dirname $0) && source ./DOCKER.variables
+
+
+if docker ps | grep -q "${CONTAINER_NAME}"; then
+  echo "Compose down the service container '${CONTAINER_NAME}' ..."
+  cd ../ && \
+  docker-compose --env-file ./docker/DOCKER.variables \
+    down -v && \
+  docker volume prune -f
+fi
---
+++ MovieRating/BackEnd/docker/compose-up.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+cd $(dirname $0) && source ./DOCKER.variables
+
+
+BUILD_OPT="--build"
+if docker images | grep -q "${IMAGE_NAME}"; then
+  BUILD_OPT=""
+fi
+
+
+if docker ps -a | grep -q "${CONTAINER_NAME}"; then
+  echo "The service container '${CONTAINER_NAME}' already exists."
+else
+  echo "Compose up the service container '${CONTAINER_NAME}' ..."
+
+  cd ../ && \
+  docker-compose --env-file ./docker/DOCKER.variables \
+    up ${BUILD_OPT} -d
+fi
