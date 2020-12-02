9e8dbbf403d1a9aa0d766a4b908078bf15193bd4
Zhaoping Yu <yuzhaoping1970@gmail.com>
2020-12-02 12:11:31 -0500

    Handle docker env variables in the entrypoint

--- MovieRating/BackEnd/docker-entrypoint.sh
+++ MovieRating/BackEnd/docker-entrypoint.sh
@@ -2,6 +2,21 @@
 set -eo pipefail
 
 
+export ENABLE_MOCK_DB="${ENABLE_MOCK_DB:-true}"
+
+
+MONGODB_URL=""
+
+if [ -n "${MONGODB_HOSTNAME:-}" ] && \
+   [ -n "${MONGO_DB_NAME:-}" ] && \
+   [ -n "${MONGO_DB_USERNAME:-}" ] && \
+   [ -n "${MONGO_DB_PASSWORD:-}" ]; then
+ MONGODB_URL="mongodb://${MONGO_DB_USERNAME}:${MONGO_DB_PASSWORD}@${MONGODB_HOSTNAME}:27017/${MONGO_DB_NAME}"
+fi
+
+export MONGODB_URL
+
+
 if [ "${1:0:1}" = '-' ]; then
   set -- start-app "$@"
 fi
